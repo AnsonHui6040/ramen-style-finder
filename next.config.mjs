7e8ecdfeb16a67aa3ac328cb00b6da1b76ce97d2
AnsonHui6040 <ansonhui123.com@gmail.com>
2026-05-12 16:44:59 +0800

    bugfix5121644

--- next.config.mjs
+++ next.config.mjs
@@ -27,8 +27,8 @@ const repoName = "ramen-style-finder";
 
 const nextConfig = {
   reactStrictMode: true,
-  output: isGitHubPages ? "export" : "standalone",
-  trailingSlash: isGitHubPages,
+  output: "export",
+  trailingSlash: true,
   images: { unoptimized: true },
   basePath: isGitHubPages ? `/${repoName}` : "",
   assetPrefix: isGitHubPages ? `/${repoName}/` : "",
